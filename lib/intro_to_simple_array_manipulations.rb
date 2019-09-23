ba9d35a705828210fcbd75f726d3ddffa7cade70
Cane Rosso <astor@usa.com>
2019-09-23 07:14:31 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -8,7 +8,7 @@ def using_unshift(array,string)
 end
 
 def using_pop(array)
-  array.pop;#
+  array.pop;#the pop method removes the last element from the array and returns that element (not the new array)
 end
 
 def pop_with_args()
